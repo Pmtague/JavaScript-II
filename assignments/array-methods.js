391741f1aabf06c6ad7d7a735525a925b07796dd
Penny Tague <pmtague9@gmail.com>
2019-06-11 16:01:36 -0700

    Closure and most of array methods complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,28 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(item => fullName.push(item.first_name + ' ' + item.last_name))
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(function(name) {
+    return name.first_name.toUpperCase();
+  })
+  console.log(allCaps);
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(size => size.shirt_size === 'L')
+console.log(largeShirts);
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(total,sales) {
+    return total + sales.donation;
+  }, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -78,6 +85,13 @@ console.log(ticketPriceTotal);
 
 // Problem 1
 
+////The event director needs to be able to notify all runners of the details of the upcoming 5k. Grab a list of the email addresses so that info can be sent at once.
+let emailList = []
+runners.forEach(email => emailList.push(email.email))
+console.log(emailList)
+
 // Problem 2
+
+////
 
 // Problem 3
